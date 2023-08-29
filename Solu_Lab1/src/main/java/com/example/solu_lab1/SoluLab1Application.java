f80a858145812d39b039c0b75b51b8b27c420a7f
Willy Huallpa <willy.huallpa@pucp.edu.pe>
2023-08-28 19:04:57 -0500

    creacion de archivo

---
+++ Solu_Lab1/src/main/java/com/example/solu_lab1/SoluLab1Application.java
@@ -0,0 +1,13 @@
+package com.example.solu_lab1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoluLab1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoluLab1Application.class, args);
+    }
+
+}
