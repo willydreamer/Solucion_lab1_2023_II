e1aa9c174293603fa165a8e468341388779116d9
Willy Huallpa <willy.huallpa@pucp.edu.pe>
2023-08-28 19:24:12 -0500

    navbar terminado

---
+++ Solu_Lab1/src/main/java/com/example/solu_lab1/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.solu_lab1.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping(value = {"","/navbar"})
+    public String navbarView(){
+        return "index";
+    }
+}
